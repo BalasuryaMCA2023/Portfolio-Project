485902df73dabc664bc149e67c1f8b6482324f5b
BALASURYA S <139736173+BalasuryaMCA2023@users.noreply.github.com>
2023-10-20 10:48:21 +0530

    Add files via upload

---
+++ assets/vendor/contact.js
@@ -0,0 +1,29 @@
+const contactForm=document.getElementById('contact-form'),
+contactMessage= document.getElementById('contact-message')
+
+const sendEmail  = (e) =>{
+    e.preventDefault()
+    
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_bvxrwsp','template_uff18uq','#contact-form','HydokGBp1cIz1rK1I')
+    .then(() =>{
+    // Show sent message
+      contactMessage.textContent="Message sent successfully ✅"
+     
+    //Remove message after five seconds
+    setTimeout(() =>{
+        contactMessage.textContent =""
+    },5000)   
+    
+    // Clear input fields
+    contactForm.reset()
+
+},()=>{
+    //Show error message
+    contactMessage.textCOntent='Message not sent (service error) ❌'
+})
+
+
+}
+
+contactForm.addEventListener('submit',sendEmail)
